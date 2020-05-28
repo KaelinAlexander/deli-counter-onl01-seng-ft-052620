24831af95b8f1af3b35d8f0d3b20afce13d908aa
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 23:31:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli,name)
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
 
-def now_serving(katz_deli,name)
+def now_serving(katz_deli)
   if katz_deli.count>0
     puts "Currently serving #{katz_deli[0]}!"
     katz_deli.shift
