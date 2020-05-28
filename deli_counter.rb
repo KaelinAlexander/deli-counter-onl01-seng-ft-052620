71d4b6cc82a4b5a8a4666a3bbe69f43c513b7bc2
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 22:56:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.count==0
+  puts "The line is currently empty."
+else
+  puts
+  "The line is currently: #{katz_deli.count}. #{katz}."
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli,name)
+  if katz_deli.count>0
+    puts "Currently serving #{katz_deli[0]}!"
+  else
+    puts "No one is waiting to be served."
+  end
+end
