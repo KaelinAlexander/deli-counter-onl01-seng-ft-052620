450bb7b689ad35336fdc01743e43d64efaa54705
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 23:26:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
 current_line=[]
 if katz_deli.count>0
   katz_deli.each do |customer|
-    puts "The line is currently "
+  "#{kate_deli("
   end
   puts "The line is currently: #{current_line}."
 else
